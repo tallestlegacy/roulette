eebb811132810d4c388c85ecc3280f13ce707607
Marvin <marvinmokua@gmail.com>
2022-11-12 17:57:39 +0300

    init

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	//const emojis = "😀😁😜😋🤯🥺".split("");
+
+	const emojis = "supercalifragilisticexpialidocious".split("");
+	let index = 0;
+	$: emoji = emojis[index];
+
+	function increment() {
+		if (index < emojis.length - 1) {
+			index++;
+		} else {
+			index = 0;
+		}
+	}
+	setInterval(increment, 500);
+</script>
+
+<span>{emoji}</span>
+.
+<span>{index}</span>
+
+<style>
+	span {
+		font-size: 10rem;
+		font-family: "Courier New", Courier, monospace;
+	}
+</style>
